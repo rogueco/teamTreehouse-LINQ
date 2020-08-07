fae51e59d8bfc489858a5c8e304a6fc07b250ab4
TomFletcherPersonal <tom@tom-fletcher.co.uk>
2020-08-07 11:26:59 +0100

    first commit

---
+++ SectionOne/FirstLinqQuery/FirstLinqQuery/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstLinqQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var numbers = new List<int> { 2, 4, 7, 16, 32, 64 };
+
+            Console.WriteLine($"A full list of numbers are: {numbers}");
+
+
+            var numbersGreaterThanTen = new List<int>();
+
+            foreach(int number in numbers)
+            {
+                if (number > 10)
+                {
+                    numbersGreaterThanTen.Add(number);
+                    Console.WriteLine($"All the numbers which are greater than ten are: {number} ");
+                }
+            }
+
+
+
+            var q = from number in numbers where number > 10 select number; 
+
+            foreach (int item in numbers) 
+            { 
+                Console.WriteLine(item.ToString()); 
+            }
+        }
+    }
+}
